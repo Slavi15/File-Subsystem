c3661c46fe29d6be4b7157cbacbdcf25ac0a97a1
Slavi15 <slavirezashkiofficial@gmail.com>
2024-12-08 18:23:37 +0200

    Update printing of directories

--- src/Printer.hs
+++ src/Printer.hs
@@ -7,6 +7,6 @@ printFile (MkFile name content) = "File: " ++ name ++ "\nContent: \n" ++ content
 printFile _ = ""
 
 printDirectory :: [FileSystem] -> String
--- printDirectory ((MkFile "/" _) : fs) = "/" ++ printDirectory fs
-printDirectory ((MkFile name _) : fs) = name ++ "/" ++ printDirectory fs
+printDirectory ((MkDirectory "/" _) : fs) = "/" ++ printDirectory fs
+printDirectory ((MkDirectory name _) : fs) = name ++ "/" ++ printDirectory fs
 printDirectory _ = ""
