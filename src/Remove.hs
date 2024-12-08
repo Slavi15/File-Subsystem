26292f3fafdd87585cd78c7bfc0d83d7ebf33528
Slavi15 <slavirezashkiofficial@gmail.com>
2024-12-08 17:52:21 +0200

    Add logic for removing files v1.0 (only supports rmeoving files or directories from current directory)

--- src/Remove.hs
+++ src/Remove.hs
@@ -1,3 +1,25 @@
 module Remove where
 
--- for rm
+import Data.FileSystem ( FileSystem(..) )
+
+import Utility (getName)
+
+rm :: [FileSystem] -> Maybe FileSystem -> Maybe FileSystem
+rm toBeRemoved (Just (MkDirectory name contents)) =
+    Just $ MkDirectory name (filter (\entry -> getName entry `notElem` removeNames) contents)
+    where
+        removeNames :: [String]
+        removeNames = map getName toBeRemoved
+rm _ fs = fs
+
+rmFile :: String -> Maybe FileSystem -> Maybe FileSystem
+rmFile input = rm files
+    where
+        files :: [FileSystem]
+        files = map (`MkFile` "") (words input)
+
+rmDirectory :: String -> Maybe FileSystem -> Maybe FileSystem
+rmDirectory input = rm dirs
+    where
+        dirs :: [FileSystem]
+        dirs = map (`MkDirectory` []) (words input)
