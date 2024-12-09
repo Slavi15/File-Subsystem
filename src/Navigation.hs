11d0679eafe9b138596a4f1356630106fcaf6ecc
Slavi15 <slavirezashkiofficial@gmail.com>
2024-12-09 18:15:04 +0200

    Add Navigation logic v1.0

--- src/Navigation.hs
+++ src/Navigation.hs
@@ -1,3 +1,46 @@
+{-# LANGUAGE LambdaCase #-}
 module Navigation where
 
--- for cd, ls, pwd
+import Core.FileSystem ( FileSystem(..) )
+
+import Data.Maybe (maybeToList, listToMaybe)
+import Data.List (find)
+
+import Parser (getNextDirectory)
+import Output (printDirectory, printEntity, printCDCommand)
+import Utility (getName)
+
+pwd :: Maybe FileSystem -> Maybe FileSystem -> String
+pwd fs = pwdHelper (getName fs)
+
+pwdHelper :: String -> Maybe FileSystem -> String
+pwdHelper name fs = printDirectory name (maybeToList fs)
+
+cd :: String -> [FileSystem] -> Maybe FileSystem
+cd input fs = case getNextDirectory input of
+    Just ("", "") -> listToMaybe fs
+    Just (rest, "..") ->
+        case goToParentDirectory fs of
+            (Just parentDirectory) -> cd rest parentDirectory
+            Nothing -> Nothing
+    Just (rest, curr) ->
+        case goToSubDirectory curr fs of
+            (Just subDirectory) -> cd rest (subDirectory : fs)
+            Nothing -> Nothing
+    Nothing -> Nothing
+
+ls :: String -> Maybe FileSystem -> String
+ls input fs = case cd input (maybeToList fs) of
+    (Just (MkDirectory _ contents)) -> concatMap printEntity contents
+    Nothing -> "Invalid directory!\n"
+
+goToParentDirectory :: [FileSystem] -> Maybe [FileSystem]
+goToParentDirectory (_ : fs) = Just fs
+goToParentDirectory [] = Nothing
+
+goToSubDirectory :: String -> [FileSystem] -> Maybe FileSystem
+goToSubDirectory name (MkDirectory _ contents : _) = 
+    find (\case
+        MkDirectory directoryName _ -> name == directoryName
+        _ -> False) contents
+goToSubDirectory _ _ = Nothing
