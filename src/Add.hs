5e5c4090ea44ca99c65f15e2b0b8ed739afa223f
Slavi15 <slavirezashkiofficial@gmail.com>
2025-01-18 16:10:32 +0200

    Update file and directory adding with paths

--- src/Add.hs
+++ src/Add.hs
@@ -8,10 +8,10 @@ import Parser (wordParser, getNextDirectory, eofParser)
 import Utility (isPath)
 
 mk :: String -> [FileSystem] -> [FileSystem] -> Maybe [FileSystem]
-mk "" toBeAdded ((MkDirectory name contents) : fs) =
-    Just (MkDirectory name (nub $ contents ++ toBeAdded) : fs)
 mk path toBeAdded ((MkDirectory name contents) : fs) =
     case getNextDirectory path of
+        Just ("", "") ->
+            Just (MkDirectory name (nub $ contents ++ toBeAdded) : fs)
         Just (rest, curr) ->
             case updateContent curr rest toBeAdded contents of
                 (Just updatedContents) -> Just (MkDirectory name updatedContents : fs)
@@ -39,20 +39,22 @@ mkDirectory :: String -> [FileSystem] -> Maybe [FileSystem]
 mkDirectory input fs = case wordParser input of
     Just (rest, curr) ->
         if isPath curr
-            then mk curr parsedFolders fs
-            else mk "" (MkDirectory curr [] : parsedFolders) fs
-        where
-            parsedFolders = map (`MkDirectory` []) (words rest)
+            then mk curr (parseFolders rest) fs
+            else mk "" (MkDirectory curr [] : parseFolders rest) fs
     Nothing -> Nothing
+
+    where
+        parseFolders :: String -> [FileSystem]
+        parseFolders rest = map (`MkDirectory` []) (words rest)
 
 updateContent :: String -> String -> [FileSystem] -> [FileSystem] -> Maybe [FileSystem]
 updateContent targetPath restPath toBeAdded (entry@(MkDirectory name contents) : fs)
     | name == targetPath =
-        case mk restPath toBeAdded contents of
-            (Just updatedContents) -> Just (MkDirectory name updatedContents : fs)
+        case mk restPath toBeAdded (entry : fs) of
+            (Just updatedContents) -> Just updatedContents
             Nothing -> Nothing
     | otherwise =
-        case updateContent targetPath restPath toBeAdded fs of
+        case updateContent targetPath restPath toBeAdded contents of
             (Just remaining) -> Just (entry : remaining)
             Nothing -> Nothing
 updateContent _ _ _ [] = Nothing
